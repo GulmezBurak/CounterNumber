7485b8e998ea125c14fbce340ef1a45a8a369057
BURAK GULMEZ <burakgulmez34@gmail.com>
2023-03-08 19:17:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+let count = 0;
+
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else if (styles.contains("increaseDouble")) {
+      count *= 2;
+    } else if (styles.contains("decreaseDouble")) {
+      count /= 2;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#211";
+    }
+    value.textContent = count;
+  });
+});
